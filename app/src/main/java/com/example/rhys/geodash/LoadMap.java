7ee062f85df473076732def38b1e06b6f58acc01
Kelsey LaPointe <i4v5p@unb.ca>
2016-03-31 22:19:38 -0300

    Started Recyclerview
    Recyclerview implementtion started. Used skeleton from lab exam. Need to
    pull data from map. THERE ARE ERRORS HERE

--- app/src/main/java/com/example/rhys/geodash/LoadMap.java
+++ app/src/main/java/com/example/rhys/geodash/LoadMap.java
@@ -1,22 +1,25 @@
 package com.example.rhys.geodash;
 
-import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.GoogleMap;
+
+import java.util.ArrayList;
 
 /**
  * Created by Kelsey on 3/31/2016.
@@ -34,6 +37,7 @@ public class LoadMap extends AppCompatActivity {
     private Button contBtn;
     private Button backBtn;
     private static final String[] ITEM_NUM_COLS = { DBHelper.NAME, DBHelper.CITY, DBHelper.TIMELIMIT, DBHelper.PRIVATE };
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +63,13 @@ public class LoadMap extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        // Get a reference to the RecyclerView and configure it
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
 
+        LoadDataTask task = new LoadDataTask();
+        task.execute();
 
     }
 
@@ -73,6 +83,90 @@ public class LoadMap extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
 
+    }
+
+    //TODO - Complete the RecyclerView Adapter
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private ArrayList<DictionaryEntry> mDataset;
+
+        public MyAdapter(ArrayList<DictionaryEntry> myDataset) {
+            mDataset = myDataset;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView mTextView;
+
+            public ViewHolder(TextView v) {
+                super(v);
+                mTextView = v;
+            }
+        }
+
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_layout, parent, false);
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // TODO Get the DictionaryEntry at index position in mDataSet
+            // You might need to declare this variable as final.
+            final DictionaryEntry entry = mDataset.get(position);
+
+
+            // TODO Set the TextView in the ViewHolder (holder) to be the
+            // word in this DictionaryEntry
+            holder.mTextView.setText(entry.getWord());
+
+            // TODO Set the onClickListener for the TextView in the ViewHolder (holder) such
+            // that when it is clicked, it creates an explicit intent to launch DetailActivity
+            // HINT: You will need to put two extra pieces of information in this intent,
+            // the word, and its definition
+
+            holder.mTextView.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, DetailActivity.class);
+                    intent.putExtra(DetailActivity.WORD, entry.getWord());
+                    intent.putExtra(DetailActivity.DEFINITION, entry.getDefinition());
+                    context.startActivity(intent);
+                }
+            });
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDataset.size();
+        }
+    }
+
+    public class LoadDataTask extends AsyncTask<Void, Void, ArrayList<Map>> {
+        private ArrayList<Map> entryList;
+
+        protected ArrayList<Map> doInBackground(Void... params) {
+            // TODO Use DataModel to load the data from the JSON assets file
+            // and return the ArrayList of DictionaryEntrys
+            //DataModel model = new DataModel(getApplicationContext());
+            //entryList = model.getEntries();
+
+            return entryList;
+        }
+
+        protected void onPostExecute(ArrayList<Map> result) {
+            // TODO Use result to set the adapter for the RecyclerView in MainActivity
+            RecyclerView.Adapter mAdapter = new MyAdapter(result);
+            mRecyclerView.setAdapter(mAdapter);
+
+
+        }
     }
 
 
