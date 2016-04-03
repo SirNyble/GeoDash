be8b04a1c034751215b326bf2a27eda500d3b830
Kelsey LaPointe <i4v5p@unb.ca>
2016-04-03 13:58:57 -0300

    Fixed Map Detail
    No longer errors out. Still showing null info though.

--- app/src/main/java/com/example/rhys/geodash/MapDetail.java
+++ app/src/main/java/com/example/rhys/geodash/MapDetail.java
@@ -3,6 +3,7 @@ package com.example.rhys.geodash;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 /**
@@ -19,28 +20,21 @@ public class MapDetail extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_layout);
 
-        // TODO Get the intent that started this activity, and get the word and definition
-        // extras from it.
         String w = getIntent().getStringExtra(NAME);
         String d = getIntent().getStringExtra(TIME);
         String r = getIntent().getStringExtra(NUMRIDDLES);
 
-
-        // TODO Set the definition TextView to be the definition
         TextView defT = (TextView) findViewById(R.id.time);
         defT.setText(d + " minutes");
 
         TextView rid = (TextView) findViewById(R.id.numRiddles);
         rid.setText(r + " riddles");
 
-        // TODO Set the title of the action bar to be the word
-        //
-        // Hint: Note the location of the word (being defined) in the detail activity in
-        // the lab exam write-up. This portion of an Activity is the action bar. You can get
-        // a reference to the action bar here using getSupportActionBar(). There is a link to
-        // documentation on this method in the "Hints" section of the write-up.
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
         ActionBar bar = getSupportActionBar();
         bar.setTitle(w);
+        bar.setDisplayHomeAsUpEnabled(true);
 
     }
 }
