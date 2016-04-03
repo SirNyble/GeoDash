bb4a4f6c9429fd9d88fbff7e89ba0072c69fe2f6
SirNyble <thomasrhys14@gmail.com>
2016-04-03 14:16:31 -0300

    Fixing issue that recycler view displayed null values on new intent

--- app/src/main/java/com/example/rhys/geodash/MapDetail.java
+++ app/src/main/java/com/example/rhys/geodash/MapDetail.java
@@ -15,25 +15,34 @@ public class MapDetail extends AppCompatActivity {
     public static final String TIME = "time";
     public static final String NUMRIDDLES = "NUMRIDDLES";
 
+    private String mMapName = "";
+    private int mNumRiddles = 0;
+    private int mTimeLimit = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_layout);
 
-        String w = getIntent().getStringExtra(NAME);
-        String d = getIntent().getStringExtra(TIME);
-        String r = getIntent().getStringExtra(NUMRIDDLES);
+        Bundle bundleExtras = getIntent().getExtras();
+
+        if(bundleExtras != null)
+        {
+            mMapName =(String) bundleExtras.get(NAME);
+            mNumRiddles = (int) bundleExtras.get(NUMRIDDLES);
+            mTimeLimit = (int) bundleExtras.get(TIME);
+        }
 
         TextView defT = (TextView) findViewById(R.id.time);
-        defT.setText(d + " minutes");
+        defT.setText(mTimeLimit + " minutes");
 
         TextView rid = (TextView) findViewById(R.id.numRiddles);
-        rid.setText(r + " riddles");
+        rid.setText(mNumRiddles + " riddles");
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
         ActionBar bar = getSupportActionBar();
-        bar.setTitle(w);
+        bar.setTitle(mMapName);
         bar.setDisplayHomeAsUpEnabled(true);
 
     }
